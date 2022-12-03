e47d5baeb02b16289bdaf3db9b853933abf641e0
Botqueen67 <109548090+Botqueen67@users.noreply.github.com>
2022-12-04 01:44:05 +0530

    Update stream.py

--- YukkiMusic/utils/stream/stream.py
+++ YukkiMusic/utils/stream/stream.py
@@ -9,6 +9,7 @@
 
 import os
 import re
+import textwrap
 from random import randint
 from typing import Union
 
@@ -39,11 +40,42 @@ from YukkiMusic.utils.thumbnails import gen_thumb
 
 from youtubesearchpython.__future__ import VideosSearch
 
-def get_image(videoid):
+async def gen_thumb(videoid):
     if os.path.isfile(f"cache/{videoid}.png"):
         return f"cache/{videoid}.png"
-    else:
-        return config.YOUTUBE_IMG_URL
+
+    url = f"https://www.youtube.com/watch?v={videoid}"
+    try:
+        results = VideosSearch(url, limit=1)
+        for result in (await results.next())["result"]:
+            try:
+                title = result["title"]
+                title = re.sub("\W+", " ", title)
+                title = title.title()
+            except:
+                title = "Unsupported Title"
+            try:
+                duration = result["duration"]
+            except:
+                duration = "Unknown Mins"
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+            try:
+                views = result["viewCount"]["short"]
+            except:
+                views = "Unknown Views"
+            try:
+                channel = result["channel"]["name"]
+            except:
+                channel = "Unknown Channel"
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(thumbnail) as resp:
+                if resp.status == 200:
+                    f = await aiofiles.open(
+                        f"cache/thumb{videoid}.png", mode="wb"
+                    )
+                    await f.write(await resp.read())
+                    await f.close()
 
 async def stream(
     _,
