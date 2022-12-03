3f2e3c58e609319d497802a3881a61b5518a776f
Botqueen67 <109548090+Botqueen67@users.noreply.github.com>
2022-12-04 01:48:15 +0530

    Update stream.py

--- YukkiMusic/utils/stream/stream.py
+++ YukkiMusic/utils/stream/stream.py
@@ -40,43 +40,6 @@ from YukkiMusic.utils.thumbnails import gen_thumb
 
 from youtubesearchpython.__future__ import VideosSearch
 
-async def gen_thumb(videoid):
-    if os.path.isfile(f"cache/{videoid}.png"):
-        return f"cache/{videoid}.png"
-
-    url = f"https://www.youtube.com/watch?v={videoid}"
-    try:
-        results = VideosSearch(url, limit=1)
-        for result in (await results.next())["result"]:
-            try:
-                title = result["title"]
-                title = re.sub("\W+", " ", title)
-                title = title.title()
-            except:
-                title = "Unsupported Title"
-            try:
-                duration = result["duration"]
-            except:
-                duration = "Unknown Mins"
-            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-            try:
-                views = result["viewCount"]["short"]
-            except:
-                views = "Unknown Views"
-            try:
-                channel = result["channel"]["name"]
-            except:
-                channel = "Unknown Channel"
-
-        async with aiohttp.ClientSession() as session:
-            async with session.get(thumbnail) as resp:
-                if resp.status == 200:
-                    f = await aiofiles.open(
-                        f"cache/thumb{videoid}.png", mode="wb"
-                    )
-                    await f.write(await resp.read())
-                    await f.close()
-
 async def stream(
     _,
     mystic,
@@ -219,7 +182,7 @@ async def stream(
             position = len(db.get(chat_id)) - 1         
             await app.send_photo(
                 original_chat_id,
-                photo=Image.open(f"cache/thumb{videoid}.png"),
+                photo=QUEUE_IMG_URL,
                 caption=_["queue_4"].format(
                    position, title[:30], duration_min, user_name
                 ),
