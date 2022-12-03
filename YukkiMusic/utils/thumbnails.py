04db83f50e00ab2162961cdb7a033b2d6928ac4c
Botqueen67 <109548090+Botqueen67@users.noreply.github.com>
2022-12-04 00:43:03 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -87,7 +87,7 @@ async def gen_thumb(videoid):
         height,width = pogo.size
         lum_img = Image.new('L', [height,width] , 0)
         draww = ImageDraw.Draw(lum_img)
-        draww.pieslice([(0,0), (height,width)], 0, 360, fill=20)
+        draww.pieslice([(0,0), (height,width)], 0, 360, fill=255)
         img_arr = np.array(pogo)
         lum_img_arr = np.array(lum_img)
         final_img_arr = np.dstack((img_arr, lum_img_arr))
