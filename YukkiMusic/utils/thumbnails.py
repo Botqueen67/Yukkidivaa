7f4bc04646029de45b6aadb2a7109670208bbe3b
Botqueen67 <109548090+Botqueen67@users.noreply.github.com>
2022-12-03 22:26:09 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -92,17 +92,17 @@ async def gen_thumb(videoid):
         gray = cv2.cvtColor(imgg, cv2.COLOR_BGR2GRAY) # convert to grayscale
 
 # threshold to get just the signature
-        retval, thresh_gray = cv2.threshold(gray, thresh=100, maxval=255, type=cv2.THRESH_BINARY)
+        retval, thresh_gray = cv2.threshold(gray, thresh=100, maxval=0, type=cv2.THRESH_BINARY)
 
 # find where the signature is and make a cropped region
-        points = np.argwhere(thresh_gray==0) # find where the black pixels are
+        points = np.argwhere(thresh_gray==255) # find where the black pixels are
         points = np.fliplr(points) # store them in x,y coordinates instead of row,col indices
         x, y, w, h = cv2.boundingRect(points) # create a rectangle around those points
         x, y, w, h = x-10, y-10, w+20, h+20 # make the box a little bigger
         crop = gray[y:y+h, x:x+w] # create a cropped region of the gray image
 
 # get the thresholded crop
-        retval, thresh_crop = cv2.threshold(crop, thresh=200, maxval=255, type=cv2.THRESH_BINARY)
+        retval, thresh_crop = cv2.threshold(crop, thresh=200, maxval=0, type=cv2.THRESH_BINARY)
         background.paste(thresh_crop, (120, 100))
         draw = ImageDraw.Draw(background)
         font = ImageFont.truetype("assets/font2.ttf", 40)
