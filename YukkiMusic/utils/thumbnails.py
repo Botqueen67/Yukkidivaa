a19e9fe75b19919b7d4d22c2c53f5d1d4651407e
Botqueen67 <109548090+Botqueen67@users.noreply.github.com>
2022-12-03 16:36:22 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -84,7 +84,7 @@ async def gen_thumb(videoid):
         pogo = ImageOps.expand(logo, border=10, fill="orange")
         im_a = Image.new("L", pogo.size, 0)
         draww = ImageDraw.Draw(im_a)
-        draww.ellipse((135, 45, 690, 600), fill=255)        
+        draww.ellipse((135, 45, 265, 175), fill=255)        
         im_rgba = pogo.copy()        
         im_rgba.putalpha(im_a)
         background.paste(im_rgba, (170, 100))
